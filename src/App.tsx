ef34432b09115c4eaf29119a07c7a546d33507f2
Tomás Morales Galván <trabajostomasmorales@gmail.com>
2025-04-11 21:30:17 +0100

    Haiku Share Routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import AboutPage from "./pages/AboutPage";
+import HaikuByDate from "./pages/HaikuByDate";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/haiku/:date" element={<HaikuByDate />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>

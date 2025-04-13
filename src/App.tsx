2022f90e4e03319c2667f5d1b5ce9d4cac95d328
Tomás Morales Galván <trabajostomasmorales@gmail.com>
2025-04-13 04:12:58 +0100

    fix(routes): add missing /history route to router config

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import AboutPage from "./pages/AboutPage";
 import HaikuDatePage from "./pages/HaikuDatePage";
+import HistoryPage from "./pages/HistoryPage";
 
 
 const queryClient = new QueryClient();
@@ -21,6 +22,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/haiku/:date" element={<HaikuDatePage />} />
+          <Route path="/history" element={<HistoryPage />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
